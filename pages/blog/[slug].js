b3d706d6ca7921ffe23db4016b7c9b090c58e128
Bruno Vianna <desenvolvedor1@dt3.com.br>
2021-06-01 16:16:20 -0300

    Armazena apenas a url da imagem

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -105,7 +105,7 @@ export default function BlogTemplate(props) {
           label: item.name,
         }),
         defaultItem: () => ({
-          name: 'slides',
+          image: '/static/error.png',
         }),
         fields: [
           {
